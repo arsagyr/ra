2c29b0d3f9719a595b4adb6838625418ff79f52e
arsagyr <sdkalsky@gmail.com>
2025-11-27 17:54:40 +0700

    Добавлено нахождение пути

--- dynprog/bellman.go
+++ dynprog/bellman.go
@@ -4,138 +4,47 @@ import (
 	"fmt"
 )
 
-func BellmanEquationPrint(res int, table [][]int) {
-	var i, j, k int
-	var l int
-
-	rows := len(table)
-	subtable := make([]int, rows)
-	for i = 0; i < rows; i++ {
-		subtable[i] = table[i][1]
-	}
-
-	maxkn := 0
-	maxjn := 0
-	step := 0
-	finalstep := 0
-	for i = 1; i < len(table[0])-1; i++ {
-		subsubtable := make([]int, rows)
-		maxk := table[0][i] + table[0][i+1]
-		maxj := maxk
-		step = maxjn
-		for l = 0; l < res; l++ {
-			for j = 0; j < rows; j++ {
-				for k = 0; k < rows; k++ {
-					if l == (table[j][0] + table[k][0]) {
-						if subtable[j]+table[k][i+1] > maxk {
-							maxk = subtable[j] + table[k][i+1]
-							maxkn = j
-						}
-					}
-				}
-				subsubtable[l] = maxk
-				if maxj < maxk {
-					maxj = maxk
-					maxjn = maxkn
-				}
-			}
-
-		}
-		subtable = subsubtable
-		finalstep += (maxjn - step)
-		fmt.Printf("x%d = %d\n", i, maxjn-step) //Вывод шагов инвестиций
-	}
-
-	fmt.Printf("x%d = %d\n", i, rows-finalstep-1) //Вывод последнего шага
-
-	maxk := subtable[0]
-	for i = 1; i < rows; i++ {
-		if subtable[i] > maxk {
-			maxk = subtable[i]
-		}
-	}
-
-	fmt.Printf("Максимальная прибыль инвестиций - %d\n", maxk)
-}
-
-func BellmanEquation(res int, table [][]int) {
-	var i, j, k, l int
-
-	rows := len(table)
-	cols := len(table[0])
-	fmt.Printf("rows = %d\ncols = %d\n", rows, cols)
-	steps := make([]int, cols)
-	profits := make([]int, rows)
-	profits[0] = table[0][1]
-
-	var finalstep, maxkprice, maxjprice int
-	for i = 1; i < cols-1; i++ {
-		for l = 0; l < res; l++ {
-			maxj := table[0][i] + table[0][i+1]
-			maxk := table[0][i] + table[0][i+1]
-			for j = 0; j < rows; j++ {
-				for k = 0; k < rows; k++ {
-					if l == (table[j][0] + table[k][0]) {
-						if table[j][i]+table[k][i+1] > maxk {
-							maxk = profits[i-1] + table[k][i+1]
-							maxkprice = table[k][0]
-							// fmt.Printf("profits[%d] + table[%d][%d] = %d + %d = %d\n", i, k, i+1, profits[i-1], table[k][i+1], maxk)
-						}
-					}
-				}
-
-				if maxj < maxk {
-					maxj = maxk
-					maxjprice = maxkprice
-				}
-			}
-			profits[i] = maxj
-			steps[i] = maxjprice
-		}
-		fmt.Printf("step%d = %d\n", i, steps[i])
-	}
-	finalstep += maxjprice
-	fmt.Printf("x%d = %d\n", i, res-finalstep) //Вывод последнего шага
-
-	maxprofit := profits[0]
-	for i = 1; i < rows; i++ {
-		fmt.Println(profits[i])
-		if profits[i] > maxprofit {
-			maxprofit = profits[i]
-		}
-	}
-
-	fmt.Printf("Максимальная прибыль инвестиций - %d\n", maxprofit)
-}
-
-func BellmanEquationVer2(res int, table [][]int) {
+func BellmanEquation(res int, table [][]int) (int, []int) {
 	var c, i, j, l int
 
 	rows := len(table)
 	cols := len(table[0])
+	ways := make([][]int, rows)
 	profits := make([]int, rows)
 	for i = 0; i < rows; i++ {
 		profits[i] = table[i][1]
 	}
+
+	for i = 0; i < rows; i++ {
+		ways[i] = make([]int, 1)
+		ways[i][0] = i
+	}
 	for c = 1; c < cols-1; c++ {
 		subprofits := make([]int, res+1)
+		subways := make([][]int, res+1)
 		for l = 0; l <= res; l++ {
 			pairsum := table[0][c] + table[0][c+1]
 			maxpairsum := pairsum
+			maxpoints := []int{c - 1, l}
 			for i = 0; i < len(profits); i++ {
+				points := []int{}
 				for j = 0; j < rows; j++ {
 					if i+table[j][0] == l {
 						pairsum = profits[i] + table[j][c+1]
+						points = append(ways[i], j)
 					}
 				}
 				if maxpairsum < pairsum {
 					maxpairsum = pairsum
+					maxpoints = points
 				}
 			}
 			subprofits[l] = maxpairsum
+			subways[l] = maxpoints
 		}
 
 		profits = subprofits
+		ways = subways
 	}
 
 	maxprofitnum := 0
@@ -144,16 +53,16 @@ func BellmanEquationVer2(res int, table [][]int) {
 			maxprofitnum = i
 		}
 	}
+	return profits[maxprofitnum], ways[maxprofitnum]
 
-	fmt.Printf("Максимальная прибыль инвестиций - %d\n", profits[maxprofitnum])
 }
 
-func BellmanEquationVer3(res int, table [][]int) {
+func BellmanEquationPrint(res int, table [][]int) {
 	var c, i, j, l int
 
 	rows := len(table)
 	cols := len(table[0])
-	ways := make([][]int, rows, cols)
+	ways := make([][]int, rows)
 	profits := make([]int, rows)
 	for i = 0; i < rows; i++ {
 		profits[i] = table[i][1]
@@ -198,7 +107,7 @@ func BellmanEquationVer3(res int, table [][]int) {
 		}
 	}
 	for step, invest := range ways[maxprofitnum] {
-		fmt.Printf("%d, %d \n", step, invest)
+		fmt.Printf("Предприятию %d вложить %d \n", step+1, invest)
 	}
 
 	fmt.Printf("Максимальная прибыль инвестиций - %d\n", profits[maxprofitnum])
